7f549f805c8724ce39c60b61933504bf5dd2f8cf
Grace <gracelo0717@gmail.com>
2025-02-09 21:47:02 -0500

    import profile pic

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../styles.css';
+const profilePic = require('../assets/profile_pic.JPG');
 
 const App = () => {
   return (
     <div className='landing-page'>
-      <div className='landing-container'>
-        <header className='landing-header'>
-          <h1>Grace Lo</h1>
-          <p>Software Engineer</p>
-          <div className='buttons-container'>
-            <button>About</button>
-            <button>Experience</button>
-            <button>Projects</button>
-            <button>Contact</button>
-          </div>
-        </header>
-      </div>
+      <header className='landing-header'>
+        <img src={profilePic} alt='Grace' className='profile-pic' />
+        <h1>Grace Lo</h1>
+        <p>Full Stack Developer</p>
+        <div className='buttons-container'>
+          <button>About</button>
+          <button>Experience</button>
+          <button>Projects</button>
+          <button>Contact</button>
+        </div>
+      </header>
     </div>
   );
 };
