083587be7f331d7eb5ad242b2dd76621f89df14c
Grace <gracelo0717@gmail.com>
2025-02-10 16:01:12 -0500

    install and use MUI for navbar icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import WorkIcon from '@mui/icons-material/Work';
+import CodeIcon from '@mui/icons-material/Code';
+import MailIcon from '@mui/icons-material/Mail';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <Link to='/'>Home</Link>
-      <Link to='/aboutme'>About</Link>
-      <Link to='/experience'>Experience</Link>
-      <Link to='/projects'>Projects</Link>
-      <Link to='/contact'>Contact</Link>
+      <Link to='/'>
+        <HomeIcon fontSize='medium' />
+      </Link>
+      <Link to='/aboutme'>
+        <PersonIcon fontSize='medium' />
+      </Link>
+      <Link to='/experience'>
+        <WorkIcon fontSize='medium' />
+      </Link>
+      <Link to='/projects'>
+        <CodeIcon fontSize='medium' />
+      </Link>
+      <Link to='/contact'>
+        <MailIcon fontSize='medium' />
+      </Link>
     </div>
   );
 };
