08a7b46f2903ffce8fe00af8859c280fae4115d1
Grace <gracelo0717@gmail.com>
2025-03-03 16:37:58 -0500

    add resume MUI button

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
+import DescriptionIcon from '@mui/icons-material/Description';
 
 const Socials = () => {
   return (
@@ -10,7 +11,7 @@ const Socials = () => {
         href='https://www.linkedin.com/in/gracelo0717/'
         target='_blank'
         rel='noopener noreferrer'
-        style={{ color: '#6a6a6a' }}
+        style={{ color: '#5c5c5c' }}
       >
         <LinkedInIcon fontSize='large' />
       </a>
@@ -18,12 +19,25 @@ const Socials = () => {
         href='https://github.com/gracelo0717'
         target='_blank'
         rel='noopener noreferrer'
-        style={{ color: '#6a6a6a' }}
+        style={{ color: '#5c5c5c' }}
       >
         <GitHubIcon fontSize='large' />
       </a>
-      <a href='mailto:Lo.grace717@gmail.com' style={{ color: '#6a6a6a' }}>
+      <a href='mailto:Lo.grace717@gmail.com' style={{ color: '#5c5c5c' }}>
         <EmailIcon fontSize='large' />
+      </a>
+      <a
+        href='https://docs.google.com/document/d/11ZD7eeLfxz-wEkZft38YBFTg8lsNYA-2cC5RHosQCso/edit?usp=sharing'
+        target='_blank'
+        rel='noopener noreferrer'
+        style={{
+          display: 'inline-flex',
+          alignItems: 'center',
+          textDecoration: 'none',
+          color: '#5c5c5c',
+        }}
+      >
+        <DescriptionIcon fontSize='large' />
       </a>
     </div>
   );
