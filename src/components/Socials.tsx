3eedd5d101ef34ea18e4d68a887f419c8efcc2f4
Grace <gracelo0717@gmail.com>
2025-03-03 13:26:58 -0500

    add Email icon to social

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 const Socials = () => {
   return (
@@ -20,6 +21,9 @@ const Socials = () => {
         style={{ color: '#6a6a6a' }}
       >
         <GitHubIcon fontSize='large' />
+      </a>
+      <a href='mailto:Lo.grace717@gmail.com' style={{ color: '#6a6a6a' }}>
+        <EmailIcon fontSize='large' />
       </a>
     </div>
   );
