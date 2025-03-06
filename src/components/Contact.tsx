20863047b47a07bcaa51f693360f4fc91a1b0f7f
Grace <gracelo0717@gmail.com>
2025-03-06 17:05:14 -0500

    add contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,8 +3,35 @@ import React from 'react';
 const Contact = () => {
   return (
     <section id='contact'>
-      <h2>Contact</h2>
-      <p></p>
+      <h2>Contact Me</h2>
+      <form className='contact-form'>
+        <label>Name: </label>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          placeholder='Enter your name'
+        />
+
+        <label>Email:</label>
+        <input
+          type='email'
+          id='email'
+          name='email'
+          placeholder='Enter your email'
+        />
+
+        <label>Message: </label>
+        <textarea
+          id='message'
+          name='message'
+          placeholder='Enter your message'
+        ></textarea>
+
+        <button type='button' className='submit-btn'>
+          Submit
+        </button>
+      </form>
     </section>
   );
 };
