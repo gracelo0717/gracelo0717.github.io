b5aa06595e124abb3b6829b716c98fe8b3c8da29
Grace <gracelo0717@gmail.com>
2025-03-11 13:49:35 -0400

    update acive project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import chromeImg from '../assets/chrome.png';
+
 interface Project {
   id: number;
   name: string;
@@ -26,6 +27,12 @@ const projects: Project[] = [
 ];
 
 const Projects = () => {
+  const [activeTab, setActiveTab] = useState<number>(1);
+
+  const handleTabClick = (id: number) => {
+    setActiveTab(id);
+  };
+
   return (
     <section id='projects'>
       <div className='tab-container'>
@@ -34,28 +41,34 @@ const Projects = () => {
             <img src={chromeImg} alt='Chrome' className='chrome-image' />
           </div>
           {projects.map((project) => (
-            <div key={project.id} className='tab'>
+            <div
+              key={project.id}
+              className={`tab ${activeTab === project.id ? 'active' : ''}`}
+              onClick={() => handleTabClick(project.id)}
+            >
               <span className='tab-label'>{project.name}</span>
             </div>
           ))}
           <div className='add-tab-button'>+</div>
         </div>
         <div className='tab-content'>
-          {projects.map((project) => (
-            <div key={project.id} className='project-details'>
-              <img
-                src={project.image}
-                alt={project.name}
-                className='project-image'
-              />
-              <h3>{project.name}</h3>
-              <p>{project.description}</p>
-              <p>
-                <strong>Technologies Used: </strong>
-                {project.technologies.join(', ')}
-              </p>
-            </div>
-          ))}
+          {projects.map((project) =>
+            activeTab === project.id ? (
+              <div key={project.id} className='project-details'>
+                <img
+                  src={project.image}
+                  alt={project.name}
+                  className='project-image'
+                />
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <p>
+                  <strong>Technologies Used: </strong>
+                  {project.technologies.join(', ')}
+                </p>
+              </div>
+            ) : null
+          )}
         </div>
       </div>
     </section>
