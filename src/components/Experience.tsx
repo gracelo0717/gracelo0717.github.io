98db76a2ede8c58e2be3999199e16c71f48e600a
Grace <gracelo0717@gmail.com>
2025-04-01 20:11:02 -0400

    add DeClustor to Exp

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,7 +4,15 @@ const Experience = () => {
   return (
     <section id='experience'>
       <h2>Experience</h2>
-      <p></p>
+      <div className='job'>
+        <h3>
+          Software Engineer:
+          <strong> DeClustor</strong>
+        </h3>
+        <p>
+          <strong>Duration:</strong> May 2024 – Present
+        </p>
+      </div>
     </section>
   );
 };
