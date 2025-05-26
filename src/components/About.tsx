e9c6962ea4022f767b9b285f6f4fd137e6d9c49d
Grace <gracelo0717@gmail.com>
2025-05-26 16:28:01 -0400

    change tech-stack

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -30,7 +30,7 @@ const About = () => {
             <div>
               <h3 className='tech-title'>Tech Stack</h3>
             </div>
-            <h3>Languages & Libraries</h3>
+            <h3>Full-Stack Development</h3>
             <div className='tech-grid'>
               <div className='tech-icon' title='JavaScript'>
                 <i className='devicon-javascript-plain colored'></i>
@@ -38,11 +38,8 @@ const About = () => {
               <div className='tech-icon' title='TypeScript'>
                 <i className='devicon-typescript-plain colored'></i>
               </div>
-              <div className='tech-icon' title='HTML5'>
-                <i className='devicon-html5-plain colored'></i>
-              </div>
-              <div className='tech-icon' title='CSS3'>
-                <i className='devicon-css3-plain colored'></i>
+              <div className='tech-icon' title='Python'>
+                <i className='devicon-python-plain colored'></i>
               </div>
               <div className='tech-icon' title='React'>
                 <i className='devicon-react-original colored'></i>
@@ -50,12 +47,12 @@ const About = () => {
               <div className='tech-icon' title='Redux'>
                 <i className='devicon-redux-original colored'></i>
               </div>
-              <div className='tech-icon' title='Material UI'>
-                <i className='devicon-materialui-plain colored'></i>
+              <div className='tech-icon' title='Node.js'>
+                <i className='devicon-nodejs-plain-wordmark colored'></i>
               </div>
-            </div>
-            <h3>Databases</h3>
-            <div className='tech-grid'>
+              <div className='tech-icon' title='Express'>
+                <i className='devicon-express-original colored'></i>
+              </div>
               <div className='tech-icon' title='PostgreSQL'>
                 <i className='devicon-postgresql-plain colored'></i>
               </div>
@@ -65,26 +62,29 @@ const About = () => {
               <div className='tech-icon' title='SQLite3'>
                 <i className='devicon-sqlite-plain colored'></i>
               </div>
-            </div>
-            <h3>Frameworks & Runtimes</h3>
-            <div className='tech-grid'>
-              <div className='tech-icon' title='Node.js'>
-                <i className='devicon-nodejs-plain-wordmark colored'></i>
+              <div className='tech-icon' title='HTML5'>
+                <i className='devicon-html5-plain colored'></i>
               </div>
-              <div className='tech-icon' title='Express'>
-                <i className='devicon-express-original colored'></i>
+              <div className='tech-icon' title='CSS3'>
+                <i className='devicon-css3-plain colored'></i>
               </div>
-            </div>
-            <h3>Technologies</h3>
-            <div className='tech-grid'>
-              <div className='tech-icon' title='Docker'>
-                <i className='devicon-docker-plain colored'></i>
+              <div className='tech-icon' title='Material UI'>
+                <i className='devicon-materialui-plain colored'></i>
               </div>
               <div className='tech-icon' title='D3.js'>
                 <i className='devicon-d3js-plain colored'></i>
               </div>
+            </div>
+            <h3>Tooling/Infrastructure</h3>
+            <div className='tech-grid'>
+              <div className='tech-icon' title='Docker'>
+                <i className='devicon-docker-plain colored'></i>
+              </div>
               <div className='tech-icon' title='Git'>
                 <i className='devicon-git-plain colored'></i>
+              </div>
+              <div className='tech-icon' title='Github'>
+                <i className='devicon-github-original colored'></i>
               </div>
               <div className='tech-icon' title='Webpack'>
                 <i className='devicon-webpack-plain colored'></i>
@@ -92,11 +92,17 @@ const About = () => {
               <div className='tech-icon' title='Vite'>
                 <i className='devicon-vitejs-plain colored'></i>
               </div>
-            </div>
-            <h3>AWS Infrastructure</h3>
-            <div className='tech-grid'>
               <div className='tech-icon' title='AWS'>
                 <i className='devicon-amazonwebservices-plain-wordmark colored'></i>
+              </div>
+              <div className='tech-icon' title='Jest'>
+                <i className='devicon-jest-plain colored'></i>
+              </div>
+              <div className='tech-icon' title='ESLint'>
+                <i className='devicon-eslint-plain colored'></i>
+              </div>
+              <div className='tech-icon' title='Postman'>
+                <i className='devicon-postman-plain colored'></i>
               </div>
             </div>
           </div>
