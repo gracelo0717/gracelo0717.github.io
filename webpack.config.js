7cef6146328b5d3a3f7dbdd69e6960cb2a2ea014
Grace <gracelo0717@gmail.com>
2025-05-26 15:28:21 -0400

    update webpack/build

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, 'index.html'),
+      favicon: path.resolve(__dirname, 'src/assets/favicon.ico'),
     }),
   ],
   module: {
